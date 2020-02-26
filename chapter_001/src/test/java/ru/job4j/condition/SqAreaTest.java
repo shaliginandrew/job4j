be094e2fcb3a6888e9feda4f3020d1906920995b
shaliginandrew <shaligin.andrew@gmail.com>
2020-02-26 18:12:22 +0300

    3. Коммиты на решения задач.[#18069]

---
+++ chapter_001/src/test/java/ru/job4j/condition/SqAreaTest.java
@@ -0,0 +1,17 @@
+package job4j.condition;
+
+import org.junit.Assert;
+import org.junit.Test;
+import ru.job4j.condition.SqArea;
+
+
+public class SqAreaTest {
+    @Test
+    public void square() {
+        int p = 6;
+        int k=2;
+        int expected = 2;
+        double out =  SqArea.square(p,k);
+        Assert.assertEquals(expected, out,0.01);
+    }
+}
