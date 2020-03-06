3a6a02ad7d800b162debb29fab3b75482c45701c
shaliginandrew <shaligin.andrew@gmail.com>
2020-03-06 17:28:15 +0300

    Заполнение массива.[#242490]

--- chapter_001/src/main/java/ru/job4j/array/ArrayDefinition.java
+++ chapter_001/src/main/java/ru/job4j/array/ArrayDefinition.java
@@ -2,8 +2,16 @@ package ru.job4j.array;
 
 public class ArrayDefinition {
     public static void main(String[] args) {
-        short[] ages=new short[10];
-        String[] surname=new String[100500];
-        float[] prices=new float[40];
+        short[] ages = new short[10];
+        String[] surname = new String[100500];
+        float[] prices = new float[40];
+        String[] names = new String[4];
+        names[0] = "Petr Arsentev";
+        names[1] = "Andrey Shalygin";
+        names[2] = "Steve Jobs";
+        names[3] = "Bill Gates";
+        for (int i = 0; i < 4; i++) {
+            System.out.println(names[i]);
+        }
     }
 }
