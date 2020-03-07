ba208fae8f8da368406157ccc43f8fa0b088761a
shaliginandrew <shaligin.andrew@gmail.com>
2020-03-07 18:30:15 +0300

    Поиск минимума в диапазоне.[#242486]

--- chapter_001/src/main/java/ru/job4j/array/Min.java
+++ chapter_001/src/main/java/ru/job4j/array/Min.java
@@ -13,7 +13,7 @@ public class Min {
     }
 
     public static void main(String[] args) {
-        int[] array = new int[] {8,3,7,2,5,6};
+        int[] array = new int[] {8,3,7,2,5,6,1};
         int result = findMin(array);
         System.out.println(result);
 
