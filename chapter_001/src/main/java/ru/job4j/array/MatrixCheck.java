750075c4a632ecc5826a2a8dc307591c0ebb35a8
shaliginandrew <shaligin.andrew@gmail.com>
2020-03-07 22:11:32 +0300

    Моно столбец в матрице.[#242488]

--- chapter_001/src/main/java/ru/job4j/array/MatrixCheck.java
+++ chapter_001/src/main/java/ru/job4j/array/MatrixCheck.java
@@ -16,14 +16,31 @@ public class MatrixCheck {
         return result;
     }
 
+    public static boolean monoVertical(char[][] board, int column) {
+        boolean result = false;
+        int count=0;
+        for (int i=0;i<3.;i++ ) {
+            if (board[i][column] == 'X') {
+                count++;
+            }
+        }
+        if (count==(board.length)) {
+            result = true;
+        }
+        return result;
+    }
+
     public static void main(String[] args) {
 
         char[][] board = new char[][] {
-                {'r', 'd', 'j'},
-                {'X','X','X'},
-                {'f', 'g', 't'}
+                {'r', 'X', 'j'},
+                {'м','X','f'},
+                {'f', 'X', 't'}
         };
         boolean result=MatrixCheck.monoHorizontal(board,1);
-        System.out.println(result);
+        System.out.println("Все одинаковые символы в строке: "+result);
+        boolean result2=MatrixCheck.monoVertical(board,1);
+        System.out.println("Все одинаковые символы в столбце: "+result2);
+
     }
 }
