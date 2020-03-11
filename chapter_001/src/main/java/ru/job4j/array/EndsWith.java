41f4da5175fb1f96cd1d01905e17dfc3b0728462
shaliginandrew <shaligin.andrew@gmail.com>
2020-03-11 14:53:18 +0300

    Слово заканчивается с ...[#242482](v.2) [#242478]

--- chapter_001/src/main/java/ru/job4j/array/EndsWith.java
+++ chapter_001/src/main/java/ru/job4j/array/EndsWith.java
@@ -2,21 +2,15 @@ package ru.job4j.array;
 
 public class EndsWith {
     public static boolean endsWith(char[] word, char[] post) {
-        boolean result = false;
+        boolean result = true;
         // проверить. что массив word имеет последние элементы одинаковые с post
-        int count = 0;
         for (int i = (post.length - 1); i >= 0; i--) {
-
-                if (post[i] == (word[(word.length - post.length) + i])) {
-                    count++;
+                if (post[i] != (word[(word.length - post.length) + i])) {
+                    result=false;
+                    break;
                 }
             }
-
-            if (count == post.length) {
-                result = true;
-            }
             return result;
-
         }
 
     public static void main(String[] args) {
