34e095f1d73de648fa6cc7e71aa2f61e831b7800
shaliginandrew <shaligin.andrew@gmail.com>
2020-03-16 18:43:00 +0300

    Простое число v2.[#242549]

--- chapter_001/src/main/java/ru/job4j/loop/CheckPrimeNumber.java
+++ chapter_001/src/main/java/ru/job4j/loop/CheckPrimeNumber.java
@@ -2,26 +2,20 @@ package ru.job4j.loop;
 
 public class CheckPrimeNumber {
     public static boolean check(int number) {
-        int a=0;//счетчик делений без остатка
-        boolean prime=false;
-        for(int i=1;i<=number;i++) {
+        boolean prime = true;
+        if (number==1){
+            prime = false;
+        }
+        for (int i = 2; i < number; i++) {
             if (number % i == 0) {
-                a++;
- //выходим из цикла,как только встретили два деления без остатка и последнее
-// деление не есть загаданное число,таким образом не перебираем цикл до конца
-// либо , если загаданное число 1
-                if (((a == 2) && (i != number))|(number==1)) {
-                    prime = false;
-                    break;
-                } else {
-                    prime = true;
-                }
+                prime = false;
+                break;
             }
         }
-     return prime;
+        return prime;
     }
     public static void main(String[] args) {
-       boolean result=CheckPrimeNumber.check(11);
+       boolean result=CheckPrimeNumber.check(6);
         System.out.println(result);
     }
 }
