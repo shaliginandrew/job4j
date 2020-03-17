391ee9fc39435008dbe58456aa9615cf4cd10791
shaliginandrew <shaligin.andrew@gmail.com>
2020-03-17 19:23:13 +0300

    Крест в псевдографике. [#242550]

--- chapter_001/src/main/java/ru/job4j/loop/Slash.java
+++ chapter_001/src/main/java/ru/job4j/loop/Slash.java
@@ -4,9 +4,9 @@ public class Slash {
     public static void draw(int size) {
         for (int row = 0; row < size; row++) {
             for (int cell = 0; cell < size; cell++) {
-                boolean left = (cell + row) % 2 == 0; // добавить условие, по которому нужно определить ставить ли символ или нет.
-               boolean right = (((cell + row) % 2 == 0) & (((size % 2) != 1))); // добавить условие, что нужно ставить элемент в правый угол.
-                if (left) {
+               boolean left = (cell == row); // добавить условие, по которому нужно определить ставить ли символ или нет.
+               boolean right = (row + cell) == (size - 1); // добавить условие, что нужно ставить элемент в правый угол.
+                if (left)   {
                     System.out.print("0");
                 }  else if (right) {
                     System.out.print("0");
@@ -16,13 +16,10 @@ public class Slash {
             }
             System.out.println();
         }
-        System.out.println();
     }
 
     public static void main(String[] args) {
         System.out.println("Draw by ");
         draw(5);
-      //  System.out.println("Draw by 5");
-       // draw(5);
     }
 }
