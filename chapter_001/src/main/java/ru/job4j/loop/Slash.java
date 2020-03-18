ac78342aac39833913c058e31291af8a79b60316
shaliginandrew <shaligin.andrew@gmail.com>
2020-03-18 15:54:27 +0300

    Крест в псевдографике. [#242550]

--- chapter_001/src/main/java/ru/job4j/loop/Slash.java
+++ chapter_001/src/main/java/ru/job4j/loop/Slash.java
@@ -6,9 +6,7 @@ public class Slash {
             for (int cell = 0; cell < size; cell++) {
                boolean left = (cell == row); // добавить условие, по которому нужно определить ставить ли символ или нет.
                boolean right = (row + cell) == (size - 1); // добавить условие, что нужно ставить элемент в правый угол.
-                if (left)   {
-                    System.out.print("0");
-                }  else if (right) {
+                if (left | right)   {
                     System.out.print("0");
                 }   else {
                     System.out.print(" ");
