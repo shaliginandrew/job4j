c0ca06417eef1ed7d86e1f25a8553fedd8bb9dd6
shaliginandrew <shaligin.andrew@gmail.com>
2020-03-24 19:37:20 +0300

    Рефакторинг - Площадь треугольника.[#242642]

--- chapter_001/src/test/java/ru/job4j/condition/TriangleTest.java
+++ chapter_001/src/test/java/ru/job4j/condition/TriangleTest.java
@@ -1,21 +1,54 @@
-package ru.job4j.condition;
-
+import org.junit.Assert;
 import org.junit.Test;
-import static org.hamcrest.core.Is.is;
-import static org.junit.Assert.assertThat;
+import ru.job4j.condition.Point;
+import ru.job4j.condition.Triangle;
 
 public class TriangleTest {
 
     @Test
-    public void whenExist() {
-        boolean result = Triangle.exist(2.0, 2.0, 2.0);
-        assertThat(result, is(true));
+    public void whenAreaHaveTest1() {
+        Point a = new Point(8, 1);
+        Point b = new Point(3, 2);
+        Point c = new Point(5, 4);
+        double expected = 6;
+        Triangle result = new Triangle(a, b, c);
+        double out = result.area();
+        Assert.assertEquals(expected, out, 0.01);
     }
 
     @Test
-    public void notExist() {
-        boolean result = Triangle.exist(2.0, 2.0, 5.0);
-        assertThat(result, is(false));
+    public void whenAreaHaveTest2() {
+        Point a = new Point(1, 1);
+        Point b = new Point(2, 5);
+        Point c = new Point(5, 3);
+        double expected = 7;
+        Triangle result = new Triangle(a, b, c);
+        double out = result.area();
+        Assert.assertEquals(expected, out, 0.01);
     }
 
+    @Test
+    public void whenAreaNotHaveTest1() {
+        Point a = new Point(0, 1);
+        Point b = new Point(0, 0);
+        Point c = new Point(0, 3);
+        double expected = -1;
+        Triangle result = new Triangle(a, b, c);
+        double out = result.area();
+        Assert.assertEquals(expected, out, 0);
+    }
+
+    @Test
+    public void whenAreaNotHaveTest2() {
+        Point a = new Point(1, 1);
+        Point b = new Point(1, 1);
+        Point c = new Point(3, 0);
+        double expected = -1;
+        Triangle result = new Triangle(a, b, c);
+        double out = result.area();
+        Assert.assertEquals(expected, out, 0);
+    }
 }
+
+
+
