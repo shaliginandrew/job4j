81d6bde0dc731b94a499e13f243c9b7b4e432145
shaliginandrew <shaligin.andrew@gmail.com>
2020-03-25 15:04:44 +0300

    Состояние объекта.[#242651]

--- chapter_002/src/main/java/ru/job4j/oop/Battery.java
+++ chapter_002/src/main/java/ru/job4j/oop/Battery.java
@@ -10,13 +10,14 @@ public class Battery {
 
     public void exchange(Battery another) {
         this.load = this.load + another.load;
+        another.load = 0;
     }
 
     public static void main(String[] args) {
         Battery first = new Battery(40);
         Battery second = new Battery(30);
-        System.out.println("Заряд первой батареи: " + first.load + " Заряд второй батареи : " + second.load);
+        System.out.println("first: " + first.load + " second : " + second.load);
         first.exchange(second);
-        System.out.println("Общий заряд:" + first.load);
+        System.out.println("first: " + first.load + " second :  " + second.load);
     }
 }
