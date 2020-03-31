c6b84ca70da9edcc88305f0d5244b5d6db4bb5f5
shaliginandrew <shaligin.andrew@gmail.com>
2020-03-31 13:26:08 +0300

    Сравнение моделей. Метод equals.[#242665]

--- chapter_002/src/main/java/ru/job4j/pojo/License.java
+++ chapter_002/src/main/java/ru/job4j/pojo/License.java
@@ -1,5 +1,6 @@
 package ru.job4j.pojo;
 import java.util.Date;
+import java.util.Objects;
 
 public class License {
 
@@ -7,6 +8,23 @@ public class License {
     private String model;
     private String code;
     private Date created;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        License license = (License) o;
+        return Objects.equals(owner, license.owner) && Objects.equals(model, license.model) && Objects.equals(code, license.code) && Objects.equals(created, license.created);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(owner, model, code, created);
+    }
 
     public String getOwner() {
         return owner;
@@ -39,4 +57,6 @@ public class License {
     public void setCreated(Date created) {
         this.created = created;
     }
+
+
 }
