99b1540a1b3e6b80a83d56af3fd1e5d782254fff
shaliginandrew <shaligin.andrew@gmail.com>
2020-04-04 13:50:07 +0300

    Scanner и чтение числа из консоли.[#242614]

--- chapter_002/src/main/java/ru/job4j/io/Matches.java
+++ chapter_002/src/main/java/ru/job4j/io/Matches.java
@@ -3,29 +3,42 @@ package ru.job4j.io;
 import java.util.Scanner;
 
 public class Matches {
+
+public int check(int select) {
+    Scanner input = new Scanner(System.in);
+    boolean run = true;
+    while (run) {
+    if (select < 4 && select > 0) {
+       run = false;
+    }  else {
+       System.out.println("Необходимо выбрать от 1 до 3 спичек, попробуйте еще раз:");
+         select = Integer.valueOf(input.nextLine());
+    }
+    }
+    return select;
+}
+
     public static void main(String[] args) {
+        Matches matches = new Matches();
         Scanner input = new Scanner(System.in);
+
         int number = 11;
         while (number > 0) {
             System.out.println("Первый игрок берет спички:");
-            int select1 = Integer.valueOf(input.nextLine());
-            if (select1 < 4) {
-                number = number - select1;
-                System.out.println("Осталось " + number + " спичек");
-                if (number == 0) {
-                    System.out.println("Победил первый игрок");
-                    break;
-                }
-                System.out.println("Второй игрок берет спички:");
-                int select2 = Integer.valueOf(input.nextLine());
-
-                number = number - select2;
-                System.out.println("Осталось " + number + " спичек");
-                if (number == 0) {
-                    System.out.println("Победил второй игрок");
-                }
-            } else {
-                System.out.println("Можно взять от 1 до 3 спичек");
+            int first = matches.check(Integer.valueOf(input.nextLine()));
+            number = number - first;
+            System.out.println("Осталось " + number + " спичек");
+            if (number == 0) {
+                System.out.println("Победил первый игрок");
+                break;
+            }
+            System.out.println("Второй игрок берет спички:");
+            int second = matches.check(Integer.valueOf(input.nextLine()));
+            number = number - second;
+            System.out.println("Осталось " + number + " спичек");
+            if (number == 0) {
+                System.out.println("Победил второй игрок");
+                break;
             }
         }
     }
