476dbd059589291dcb40587b96e9329f88c2309d
shaliginandrew <shaligin.andrew@gmail.com>
2020-04-28 17:15:20 +0300

    Сортировка[#242577]

--- chapter_002/src/main/java/ru/job4j/tracker/Item.java
+++ chapter_002/src/main/java/ru/job4j/tracker/Item.java
@@ -1,5 +1,7 @@
 package ru.job4j.tracker;
 
+import java.util.Objects;
+
 public class Item {
 
     private String id;
@@ -23,5 +25,29 @@ public class Item {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Item " + this.getName() + " :" + this.getId();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Item item = (Item) o;
+        return Objects.equals(id, item.getId());
+    }
+
+
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name);
     }
 }
