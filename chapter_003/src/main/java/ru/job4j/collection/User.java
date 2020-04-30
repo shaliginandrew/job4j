94177f2cadfeb73686f132142a745e2e82cd873f
shaliginandrew <shaligin.andrew@gmail.com>
2020-04-30 18:44:01 +0300

    JaCoCo. Процент покрытие тестами.[#242567]

--- chapter_003/src/main/java/ru/job4j/collection/User.java
+++ chapter_003/src/main/java/ru/job4j/collection/User.java
@@ -13,7 +13,8 @@ public class User implements Comparable<User> {
 
     @Override
     public int compareTo(User o) {
-        return name.compareTo(o.name) == 0 ? Integer.compare(age, o.age) : name.compareTo(o.name);
+        int rsl = name.compareTo(o.name);
+        return rsl == 0 ? Integer.compare(age, o.age) :rsl;
     }
 
     @Override
