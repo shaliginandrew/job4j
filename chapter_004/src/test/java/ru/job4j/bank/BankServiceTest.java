fa27b526a63fa7741fe138c0c62bb9b4232605af
shaliginandrew <shaligin.andrew@gmail.com>
2020-05-15 12:45:47 +0300

    Optional в банковских переводах.[#242448]

---
+++ chapter_004/src/test/java/ru/job4j/bank/BankServiceTest.java
@@ -0,0 +1,48 @@
+package ru.job4j.bank;
+
+import org.junit.Test;
+
+import java.util.Optional;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.*;
+
+public class BankServiceTest {
+
+    @Test
+    public void addUser() {
+        Optional<User> user = Optional.of(new User("3434", "Petr Arsentev"));
+        BankService bank = new BankService();
+        bank.addUser(user.get());
+        assertThat(bank.findByPassport("3434"), is(user));
+    }
+
+    @Test
+    public void whenEnterInvalidPassport() {
+        Optional<User> user = Optional.of(new User("3434", "Petr Arsentev"));
+        BankService bank = new BankService();
+        bank.addUser(user.get());
+        bank.addAccount(user.get().getPassport(), new Account("5546", 150D));
+        assertNull(bank.findByRequisite("34", "5546"));
+    }
+
+    @Test
+    public void addAccount() {
+        Optional<User> user = Optional.of(new User("3434", "Petr Arsentev"));
+        BankService bank = new BankService();
+        bank.addUser(user.get());
+        bank.addAccount(user.get().getPassport(), new Account("5546", 150D));
+        assertThat(bank.findByRequisite("3434", "5546").getBalance(), is(150D));
+    }
+
+    @Test
+    public void transferMoney() {
+        Optional<User> user = Optional.of(new User("3434", "Petr Arsentev"));
+        BankService bank = new BankService();
+        bank.addUser(user.get());
+        bank.addAccount(user.get().getPassport(), new Account("5546", 150D));
+        bank.addAccount(user.get().getPassport(), new Account("113", 50D));
+        bank.transferMoney(user.get().getPassport(), "5546", user.get().getPassport(), "113", 150D);
+        assertThat(bank.findByRequisite(user.get().getPassport(), "113").getBalance(), is(200D));
+    }
+}
