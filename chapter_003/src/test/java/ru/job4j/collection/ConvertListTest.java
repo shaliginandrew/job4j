7ec683bdba17eb6bc32372df11db3c3d130e5d9f
shaliginandrew <shaligin.andrew@gmail.com>
2020-05-15 11:04:45 +0300

    Collection API Улучшения[#242446]

--- chapter_003/src/test/java/ru/job4j/collection/ConvertListTest.java
+++ chapter_003/src/test/java/ru/job4j/collection/ConvertListTest.java
@@ -1,11 +1,11 @@
 package ru.job4j.collection;
 import org.junit.Test;
+
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 import static org.hamcrest.core.Is.is;
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertThat;
 
 public class ConvertListTest {
     @Test
@@ -13,7 +13,7 @@ public class ConvertListTest {
         List<int[]> in = new ArrayList<>();
         in.add(new int[] {1});
         in.add(new int[] {2, 3});
-        List<Integer> expect = Arrays.asList(1, 2, 3);
+        List<Integer> expect = List.of(1, 2, 3);
         assertThat(ConvertList.convert(in), is(expect));
     }
 }
